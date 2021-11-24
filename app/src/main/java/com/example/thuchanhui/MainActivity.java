eceb07c3f78d57484bba9e621138be8ad8b262f5
Chung2vn <74817945+Chung2vn@users.noreply.github.com>
2021-11-24 12:43:44 +0700

    Add files via upload

---
+++ app/src/main/java/com/example/thuchanhui/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.thuchanhui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        final Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this,"Đăng nhập thành công!", Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+}
